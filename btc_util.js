8d1677ef9878c2bef5d94e218fb50fc116bd7ad9
김권영/TANGO-A <P118580@sktelecom.com>
2017-09-13 18:30:22 +0900

    add gitignores and modified about package

--- btc_util.js
+++ btc_util.js
@@ -6,7 +6,13 @@ var fs = require('fs');
 
 exports.multiPopQueue = (queue, size) => queue.splice(0, size); // queue의 앞(0번째)부터 size만큼 제거
 
-exports.fileToQueue = (fname, queue) => JSON.parse(fs.readFileSync(fname)).forEach( e => queue.push(e) );
+exports.fileToQueue = (fname, queue) => {
+  if(!fs.existsSync(fname)) {
+    return;
+  }
+  JSON.parse(fs.readFileSync(fname)).forEach( _ => queue.push(_));
+};
+//exports.fileToQueue = (fname, queue) => JSON.parse(fs.readFileSync(fname)).forEach( e => queue.push(e) );
 
 exports.numeralPad = (number) => pad(10, numeral((number)).format('0,0'));
 
