ded2e0e2d575cb2a620308df891a18557ee7cb79
kykim791 <kykim79@facebook.com>
2017-10-09 14:34:46 +0900

    added last datetime display

--- ohlcBuilder.js
+++ ohlcBuilder.js
@@ -31,26 +31,27 @@ exports.getEmitter = () => emitter;
 // 1st: 1,2,3,4,5,6,7,8,10 =>     [1,2] [3,4] [5,6] 
 // 2nd: 4,5,6,7,8,10,11,12,13 =>  [5,6] [7,8] [9,10]
 // 3nd: 6,7,8,10,11,12,13 =>      [7,8] [9,10] [11,12] 
+var heads = [];
 
 function listener(args) {
   try {
-    var heads = [];
+
+    var coins = args;
     
-    // var coins = args;
-    var coins;  
-    var headFound = false;
     if(heads.length > 0) {
+      var headFound = false;
       coins = args.map(e => {
-        if(heads.includes(e)) {
-          headFound = true;
-        }
         if(headFound) {
           return e;
+        } else {
+          if(heads.includes(e)) {
+            headFound = true;
+            return e;
+          }
         }
+
       }).filter(e => e != undefined);
-    } else {
-      coins = args;
-    }
+    } 
     
     var coinChunks = splitArray(coins, splitSize);
     heads = coinChunks.map(e => e[0]);
@@ -64,7 +65,15 @@ function listener(args) {
     //
     // [[coinChunk, ohlc], [coinChunk, ohlc], ...]
     var ohlcInfos = coinChunks.map((e) => makeOHLCfield(e));
-    
+
+    // to verify dates
+    var dateFrom = moment(new Date(ohlcInfos[0].epoch)).tz('Asia/Seoul').format('MM-DD HH:mm');
+    var dateTo3 = moment(new Date(ohlcInfos[heads.length-3].epoch)).tz('Asia/Seoul').format('DD HH:mm');
+    var dateTo2 = moment(new Date(ohlcInfos[heads.length-2].epoch)).tz('Asia/Seoul').format('DD HH:mm');
+    var dateTo1 = moment(new Date(ohlcInfos[heads.length-1].epoch)).tz('Asia/Seoul').format('DD HH:mm');
+    logger.debug('table[' + ohlcInfos.length + '], (' + dateFrom
+                + '~' + dateTo3 + ',' + dateTo2 + ',' + dateTo1 + ')');
+  
     // ohlcInfos  = 
     // [
     //    {epoch, price, volume, date, high, low, close, open},
@@ -78,14 +87,18 @@ function listener(args) {
 }
 
 function makeOHLCfield(coins){
-  var coinInfo = JSON.parse(coins[coins.length -1]);
-  const prices = coins.map(_ => JSON.parse(_).price);
-  const volumes = coins.map(_ => JSON.parse(_).volume);
+  //var coinInfo = JSON.parse(coins[coins.length -1]);
+  
+  const coinInfos = coins.map(_ => JSON.parse(_));
+  const coinInfo = coinInfos[coinInfos.length - 1];
+  
+  const prices = coinInfos.map(_ => _.price);
+  const volumes = coinInfos.map(_ => _.volume);
 
   coinInfo.date = minuteString(coinInfo.epoch);
   coinInfo.high = prices.reduce((e1, e2) => Math.max(e1,e2));
   coinInfo.low = prices.reduce((e1, e2) => Math.min(e1,e2));
-  coinInfo.close = prices[coins.length-1];
+  coinInfo.close = prices[prices.length - 1];
   coinInfo.open = prices[0];
   coinInfo.volume = volumes.reduce((e1,e2) => (e1 + e2));
   return coinInfo;
