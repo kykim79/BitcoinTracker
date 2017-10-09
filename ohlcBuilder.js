0e1c1176084148c3c7bafcc005ad3c0b4ccadeeb
kykim791 <kykim79@facebook.com>
2017-10-09 22:55:10 +0900

    fixed the bug of coin shift logic and refactored

--- ohlcBuilder.js
+++ ohlcBuilder.js
@@ -3,6 +3,9 @@ var splitArray = require('split-array');
 var log4js = require('log4js');
 var logger = log4js.getLogger('ohlc-builder');
 
+var format = require('string-format');
+format.extend(String.prototype);
+
 // CONFIG
 const SPLIT_SIZE = 'ohlc:splitSize';
 const ConfigWatch = require("config-watch");
@@ -15,11 +18,11 @@ require('moment-timezone');
 var minuteString = (epoch) => moment(new Date(epoch)).tz('Asia/Seoul').format('YYYY-MM-DD HH:mm');
 
 configWatch.on("change", (err, config) => {
-    if (err) { throw err; }
-    if(config.hasChanged(SPLIT_SIZE)) {
-      splitSize = config.get(SPLIT_SIZE);
-      logger.warn("ohlc:splitsize has been changed to "+splitSize);
-    }
+  if (err) { throw err; }
+  if (config.hasChanged(SPLIT_SIZE)) {
+    splitSize = config.get(SPLIT_SIZE);
+    logger.warn("ohlc:splitsize has been changed to " + splitSize);
+  }
 });
 
 var selector = require('./selector.js');
@@ -28,78 +31,77 @@ selector.getEmitter().on('event', listener);
 var emitter = new events.EventEmitter();
 exports.getEmitter = () => emitter;
 
-// 1st: 1,2,3,4,5,6,7,8,10 =>     [1,2] [3,4] [5,6] 
-// 2nd: 4,5,6,7,8,10,11,12,13 =>  [5,6] [7,8] [9,10]
-// 3nd: 6,7,8,10,11,12,13 =>      [7,8] [9,10] [11,12] 
-var heads = [];
+var lastHeadCoins = [];
 
 function listener(args) {
   try {
 
-    var coins = args;
-    
-    if(heads.length > 0) {
-      var headFound = false;
-      coins = args.map(e => {
-        if(headFound) {
-          return e;
-        } else {
-          if(heads.includes(e)) {
-            headFound = true;
-            return e;
-          }
-        }
-
-      }).filter(e => e != undefined);
-    } 
-    
-    var coinChunks = splitArray(coins, splitSize);
-    heads = coinChunks.map(e => e[0]);
-    
-    // coinChunks = 
+    const coinChunks = splitArray(shiftCoins(args), splitSize);
+        // coinChunks = 
     // [
     //   [ {epoch, price, volume}, {epoch, price, volume},...(splitSize 개) ], 
     //   [ {epoch, price, volume}, {epoch, price, volume},...(splitSize 개) ],
     //   ... cnt = 12 hour 
     // ]
-    //
-    // [[coinChunk, ohlc], [coinChunk, ohlc], ...]
-    var ohlcInfos = coinChunks.map((e) => makeOHLCfield(e));
+    
+    lastHeadCoins = coinChunks.map(e => e[0]);
 
-    // to verify dates
-    var dateFrom = moment(new Date(ohlcInfos[0].epoch)).tz('Asia/Seoul').format('MM-DD HH:mm');
-    var dateTo3 = moment(new Date(ohlcInfos[heads.length-3].epoch)).tz('Asia/Seoul').format('DD HH:mm');
-    var dateTo2 = moment(new Date(ohlcInfos[heads.length-2].epoch)).tz('Asia/Seoul').format('DD HH:mm');
-    var dateTo1 = moment(new Date(ohlcInfos[heads.length-1].epoch)).tz('Asia/Seoul').format('DD HH:mm');
-    logger.debug('table[' + ohlcInfos.length + '], (' + dateFrom
-                + '~' + dateTo3 + ',' + dateTo2 + ',' + dateTo1 + ')');
-  
+    var ohlcInfos = coinChunks.map((e) => makeOHLCfield(e));
     // ohlcInfos  = 
     // [
     //    {epoch, price, volume, date, high, low, close, open},
     //    {epoch, price, volume, date, high, low, close, open}
-    //   ... cnt = 12 hour / 10
+    //    ... cnt = 12 hour / 10
     //]
+
+    validateDates(ohlcInfos);
+
     emitter.emit('event', ohlcInfos);
   } catch(exception) {
     logger.error(exception);
   }
 }
 
-function makeOHLCfield(coins){
-  //var coinInfo = JSON.parse(coins[coins.length -1]);
-  
+// 1st: 1,2,3,4,5,6,7,8,10 =>     [1,2] [3,4] [5,6] 
+// 2nd: 4,5,6,7,8,10,11,12,13 =>  [5,6] [7,8] [9,10]
+// 3nd: 6,7,8,10,11,12,13 =>      [7,8] [9,10] [11,12] 
+function shiftCoins(args) {
+  var coins = args;
+
+  if (lastHeadCoins.length > 0) {
+    const firstHeadIndex = args.findIndex(e => lastHeadCoins.includes(e));
+    coins = args.filter((e,index) => index >= firstHeadIndex);
+  }
+
+  const count = coins.length % splitSize;
+  coins.splice(-count, count);
+  return coins;
+}
+
+function makeOHLCfield(coins) {
   const coinInfos = coins.map(_ => JSON.parse(_));
   const coinInfo = coinInfos[coinInfos.length - 1];
-  
+
   const prices = coinInfos.map(_ => _.price);
   const volumes = coinInfos.map(_ => _.volume);
 
   coinInfo.date = minuteString(coinInfo.epoch);
-  coinInfo.high = prices.reduce((e1, e2) => Math.max(e1,e2));
-  coinInfo.low = prices.reduce((e1, e2) => Math.min(e1,e2));
+  coinInfo.high = prices.reduce((e1, e2) => Math.max(e1, e2));
+  coinInfo.low = prices.reduce((e1, e2) => Math.min(e1, e2));
   coinInfo.close = prices[prices.length - 1];
   coinInfo.open = prices[0];
-  coinInfo.volume = volumes.reduce((e1,e2) => (e1 + e2));
+  coinInfo.volume = volumes.reduce((e1, e2) => (e1 + e2));
   return coinInfo;
+}
+
+function validateDates(ohlcInfos) {
+  // to verify dates
+  if (ohlcInfos.length > 3) {
+    logger.debug('table[{0}], ({1} ~ {2}, {3}, {4})'
+      .format(ohlcInfos.length,
+        ohlcInfos[0].date,
+        ohlcInfos[ohlcInfos.length - 3].date,
+        ohlcInfos[ohlcInfos.length - 2].date,
+        ohlcInfos[ohlcInfos.length - 1].date));
+  }
 }
