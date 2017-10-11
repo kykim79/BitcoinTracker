0a8b90eef2f8e20632ec33f1e1e5656be746d74f
Woncherl Ha <riopapa@gmail.com>
2017-10-11 21:50:30 +0900

    changed debug infomation

--- chartFeeder.js
+++ chartFeeder.js
@@ -23,7 +23,7 @@ ohlcBuilder.getEmitter().on('event', listener);
 function listener(args) {
   try {
     args.forEach((item, index) => writeChartData(item, index == 0, index));
-    logger.debug(args.length + ' columns sent to chart ');
+    logger.debug(args.length + ' columns sent to chart');
   } catch(exception) {
     logger.error('[chartFeeder ] ' + exception);
   }
