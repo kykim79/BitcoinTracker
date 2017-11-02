6f7a16610425ce644f10f195c6b6f24dd76b61b6
KwonYoung Kim <kykim79@gmail.com>
2017-11-02 19:15:32 +0900

    리펙토링(notifier)
    - 불필요한 파라미터 제거
    - 함수 중복 제거

--- notifier.js
+++ notifier.js
@@ -1,4 +1,4 @@
-var replaceall = require("replaceall");
+var replaceall = require('replaceall');
 
 var format = require('string-format');
 format.extend(String.prototype);
@@ -6,65 +6,40 @@ format.extend(String.prototype);
 const CURRENCY = process.env.CURRENCY;
 const WEBHOOK = process.env.WEBHOOK;
 
-const TIMEZONE = 'Asia/Seoul';
-
 // LOGGER
 var log4js = require('log4js');
 var logger = log4js.getLogger('notifier:' + CURRENCY.toLowerCase());
 
 let notiType = require('./notiType.js');
 
-exports.info = (line, msg) => {
-  sendToSlack(line, notiType.INFO, msg);
-};
-
-exports.warn = (line, msg) => {
-  sendToSlack(line, notiType.WARN, msg);
-};
-
-exports.danger = (line, msg) => {
-  sendToSlack(line, notiType.DANGER, msg);
-};
-exports.attach = (line, msg) => {
-  sendToSlackAttach(line, notiType.ATTACHMENT, msg);
-};
+exports.info = (line, title) => sendToSlack(line, title, true);
+exports.warn = (line, title) => sendToSlack(line, title, true, notiType.WARN);
+exports.danger = (line, title) => sendToSlack(line, title, true, notiType.WARN);
+exports.attach = (line, title) => sendToSlack(line, title, false);
 
 let slackPost = require('slackpost');
 let post = slackPost.post(WEBHOOK);
 post.setUsername(CURRENCY).enableFieldMarkdown();
-
 const EOL = require('os').EOL;
 
-function sendToSlack(line, type=notiType.INFO, title){ 
+//var mdLine = (line, title) => '{0}{2}```{1}{2}```'.format(title, line, EOL);
+var singleLine = (line, title) => replaceall(EOL, '; ', title + ', ' + line);
+
+function sendToSlack(line, title, markdown, type=notiType.INFO){
   try {
-    post
-    .setColor(type.value)
-    .setRichText('{0}{2}```{1}{2}```'.format(title, line, EOL), true)
-    .enableUnfurlLinks()
-    .send((err) => { if (err) throw err; });
-    
-    log(line, type, title);
+    //post
+    // .setColor(type.value)
+    // .setRichText(markdown ? mdLine(line, title) : line, markdown)
+    // .enableUnfurlLinks()
+    // .send((err) => { if (err) throw err; });
+
+    log(singleLine(line, title), type);
   } catch(e) {
     logger.error(e);
   }
 }
 
-function sendToSlackAttach(line, type=notiType.INFO, title){ 
-  try {
-    post
-    .setColor(type.value)
-    .setRichText(line, false)
-    .enableUnfurlLinks()
-    .send((err) => { if (err) throw err; });
-    
-    log(line, type, title);
-  } catch(e) {
-    logger.error(e);
-  }
-}
-
-function log(line, type, msg) {
-  const m = replaceall(EOL, '; ', msg + ', ' + line);
+function log(m, type) {
   switch (type.value) {
   case notiType.INFO:
     logger.info(m);
@@ -78,5 +53,5 @@ function log(line, type, msg) {
   default :
     logger.debug(m);
     break;
-  } 
+  }
 }
