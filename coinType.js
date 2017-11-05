3ca83fb49d91a6d367fc8a945bb92006f6713355
riopapa <riopapa@gmail.com>
2017-11-05 20:59:39 +0900

    Add BTG (bitcoin Gold) cointype

--- coinType.js
+++ coinType.js
@@ -2,6 +2,6 @@
 
 let Enum = require('enum');
 
-const coinType = new Enum({ 'b': 'BTC', 'e': 'ETH', 'x': 'XRP', 'c': 'BCH' });
+const coinType = new Enum({ 'b': 'BTC', 'e': 'ETH', 'x': 'XRP', 'c': 'BCH' , 'g': 'BTG' });
 
 module.exports = Object.freeze(coinType);
