f571dca71b589f4d61642cfb99212225cd5f178f
riopapa <riopapa@gmail.com>
2017-11-14 22:05:03 +0900

    Seperate help message from welcom message, Fix env related values

--- botmanager/coinConfig.js
+++ botmanager/coinConfig.js
@@ -1,11 +1,12 @@
 const coinType = require('./coinType.js');
 const coinTypes = coinType.enums.map((c) => c.value);
 const CHART_URL = process.env.CHART_URL;
+const findKey = (value) => coinType.enums.find((e) => e.value === value).key;
 
 module.exports = class configCoin {
-    constructor(coin) {
+    constructor(coin = 'BTC') {
         try {
-            this.title = coin; // Big blue head
+            this.title = coin + ' (' + findKey(coin) + ')'; // Big blue head
             this.title_link = CHART_URL + coinTypes.indexOf(coin); // link when title was pressed
             this.text = '';
             this.fields = [];
