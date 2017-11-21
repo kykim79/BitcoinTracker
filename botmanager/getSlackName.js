66304466dde28f2b014ec5aab640545c48a827e4
riopapa <riopapa@gmail.com>
2017-11-21 12:47:22 +0900

    Restore getSlackName.js

--- botmanager/getSlackName.js
+++ botmanager/getSlackName.js
@@ -5,14 +5,18 @@ exports.user = (bot, id) => userIdToName(bot, id);
 
 function channelIdToName(bot, id) {
     let channels = bot.getChannels();
-    return (channels && channels._value && channels._value.channels)
+    let channel = (channels && channels._value && channels._value.channels)
         ? '#' + channels._value.channels.find(e => e.id === id).name
         : '';
+    console.log('channel :' + channel)
+    return channel;
 }
 
 function userIdToName(bot, id) {
     let users = bot.getUsers();
-    return (users && users._value && users._value.members)
+    let user = (users && users._value && users._value.members)
         ? users._value.members.find(e => e.id === id).name
         : '';
+    console.log('user :' + user)
+    return user;
 }
